f97327bd5aebb1a10a2573536e41562535457998
Julia Tatz <julia.tatz@childrens.harvard.edu>
2021-12-21 17:03:26 -0500

    add fn to read *.nirsraw file

--- nirscloud_julia/nirscloud_data.py
+++ nirscloud_julia/nirscloud_data.py
@@ -1,3 +1,6 @@
+from pathlib import PosixPath
+
+import numpy as np
 import pandas as pd
 import xarray as xr
 from .nirscloud_hdfs import read_fastrak_ds_from_meta, read_nirs_ds_from_meta
@@ -150,3 +153,35 @@ def combine_measurements_ds(fastrak_ds: xr.Dataset, nirs_ds: xr.Dataset) -> xr.D
         measurements.coords["measurement_trial"],
     ) = measurements.measurement.str.split("split_axis", "_", 1).transpose("split_axis", ...)
     return measurements
+
+
+def read_nirsraw(nirsraw_path: PosixPath, nirsraw_rhos: tuple, nirsraw_wavelengths: tuple):
+    if nirsraw_path.parts[:2] == ("/", "smb"):
+        nirsraw_path = PosixPath("/", "home", *nirsraw_path.parts[2:])
+    with nirsraw_path.open() as f:
+        nirsraw = np.genfromtxt(f, delimiter="\t")
+    ndet = len(nirsraw_rhos)
+    nwave = len(nirsraw_wavelengths)
+    _idx, raw_data, aux, dark, _empty = np.split(
+        nirsraw, np.add.accumulate([1] + [nwave * 3 * ndet] + [ndet] * 2), axis=1
+    )
+    assert np.allclose(np.diff(_idx[:, 0]), 1) and _idx[0, 0] == 0
+    assert _empty.size == 0
+    stacked_idx = pd.MultiIndex.from_product(
+        (nirsraw_rhos, nirsraw_wavelengths, ("ac", "dc", "phase")), names=["rho", "wavelength", "variable"]
+    )
+    stacked_data = xr.DataArray(
+        raw_data,
+        dims=("time", "stacked_rho_wavelength_variable"),
+        coords={
+            "stacked_rho_wavelength_variable": stacked_idx,
+        },
+    )
+    return (
+        stacked_data.to_unstacked_dataset("stacked_rho_wavelength_variable", 2)
+        .unstack("stacked_rho_wavelength_variable")
+        .assign(
+            aux=(("time", "rho"), aux),
+            dark=(("time", "rho"), dark),
+        )
+    )
